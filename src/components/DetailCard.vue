63143f6f6f0aafd13bb9a8347f68c7f1cf95c053
Stephen Kiely <stephenkiely87@gmail.com>
2021-09-30 10:48:51 -0500

    Some changes I had not pushed yet.  Renaming the logo, adding a Detail Card and removing the Iterator.

---
+++ src/components/DetailCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card color="nav">
+    <v-card-title class="subheading font-weight-bold">
+      {{ name }}
+    </v-card-title>
+    <v-divider></v-divider>
+    <v-list dense>
+      <template v-for="item in items"
+      >
+        <v-list-item :key="item.name" v-if="item.link" :to="item.link" link>
+          <v-list-item-content>{{ item.name }}</v-list-item-content>
+          <v-list-item-content class="align-end footer--text">
+            {{ item.value }}
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item :key="item.name" v-if="!item.link">
+          <v-list-item-content>{{ item.name }}</v-list-item-content>
+          <v-list-item-content class="align-end">
+            {{ item.value }}
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-list>
+  </v-card>
+</template>
+<script>
+export default {
+  name: 'DetailCardIteratorSingle',
+  props: {
+    items: {
+      type: Array
+    },
+    name: {
+      type: String
+    },
+  }
+}
+</script>
--- src/views/SiteDetail.vue
+++ src/views/SiteDetail.vue
@@ -1,15 +1,23 @@
 <template>
-  <v-banner>{{ slug }}</v-banner>
+  <DetailCard name="Site" :items="items"/>
 </template>
 
 <script>
-
+import DetailCard from "@/components/DetailCard"
 export default {
   name: 'SiteDetail',
   data () {
       return {
-        slug: this.$route.params.slug
+        slug: this.$route.params.slug,
+        items: [
+          {name: "Test", value: "Testing Link", link: "/"},
+          {name: "Test2", value: "Testing No Link"}
+        ]
+
       };
     },
+  components: {
+    DetailCard
+  },
 }
 </script>
